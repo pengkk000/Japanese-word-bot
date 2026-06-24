7c17cb6f803144eb654eb1e1def1aec07123f564
pengkk000 <pengkk000@gmail.com>
2026-06-25 01:32:50 +0900

    Change URL and adjust HTML fetching logic
    Updated URL for fetching quiz data and modified print statements.

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import requests
 
-url = "https://search.naver.com/search.naver?where=nexearch&query=오늘의 일본어"
+url = "https://wquiz.dict.naver.com/jakodict/today/quiz.dict"
 
-html_text = requests.get(
+html = requests.get(
     url,
-    headers={"User-Agent": "Mozilla/5.0"}
+    headers={"User-Agent":"Mozilla/5.0"}
 ).text
 
-print("길이 =", len(html_text))
-
-for keyword in [
-    "list_html",
-    "word_item",
-    "오늘의 일본어",
-    "_sap_item",
-    "page_link"
-]:
-    print(keyword, "=>", keyword in html_text)
-
-print(html_text[:3000])
-
-raise Exception("확인")
+print(html[:5000])
