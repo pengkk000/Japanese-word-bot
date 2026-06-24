3bd7a262868d48ccfbe2f202a80f4acf114d1777
pengkk000 <pengkk000@gmail.com>
2026-06-25 01:36:30 +0900

    Change URL to fetch words for target date
    Updated URL to fetch words for a specific date and added an exception raise.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 
-url = "https://wquiz.dict.naver.com/jakodict/today/quiz.dict"
+url = "https://wquiz.dict.naver.com/jakodict/today/words.dict?targetDate=20260625"
 
 html = requests.get(
     url,
-    headers={"User-Agent":"Mozilla/5.0"}
+    headers={"User-Agent": "Mozilla/5.0"}
 ).text
 
 print(html[:5000])
+
+raise Exception("확인")
